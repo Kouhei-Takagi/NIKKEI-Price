fb79d1c004fa2701b69fe47caeec09c8ea3eb346
Kouhei-Takagi <35396762+Kouhei-Takagi@users.noreply.github.com>
2022-01-11 19:58:02 +0900

    Add files via upload

---
+++ data_NIKKEI_price.py
@@ -0,0 +1,16 @@
+import pandas_datareader.data as web
+import datetime
+import csv
+
+year = '22'
+month = '01'
+
+start = datetime.datetime(2022, 1, 1)
+end = datetime.datetime(2022, 1, 31)
+tsd = web.DataReader("^N225", "yahoo", start, end)
+print(tsd)
+
+with open(f'./20220110_NIKKEI/csv/{year}{month}Close_price.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(tsd['Close'])
+
