2981b8a46802f85c2b9bc9d49f52cbfa3720eba3
Kouhei Takagi(髙木耕平) <35396762+Kouhei-Takagi@users.noreply.github.com>
2022-11-15 21:59:03 +0900

    Add files via upload

---
+++ NASDAQ_3DaysFuture.py
@@ -0,0 +1,40 @@
+import pandas_datareader.data as pdr
+import statsmodels.api as sm
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime, date, timedelta
+from dateutil.relativedelta import relativedelta
+
+today=datetime.today()
+end_date=(f"{today.year}/{today.month}/{today.day}")
+past=today - relativedelta(weeks=4)
+past_date=(f"{past.year}/{past.month}/{past.day}")
+
+start=past_date
+end=end_date
+
+NASDAQCOM=pdr.DataReader("NASDAQCOM", "fred", start, end).dropna()
+ln_NASDAQCOM=np.log(NASDAQCOM.dropna())
+ln_NASDAQCOM.columns=['Close']
+y=ln_NASDAQCOM
+x=range(len(ln_NASDAQCOM))
+x=sm.add_constant(x)
+model=sm.OLS(y, x)
+results=model.fit()
+
+plt.plot(y, label='Close', color="blue")
+results.fittedvalues.plot(label='prediction', style='--')
+plt.ylabel('log(NASDAQ Composite Index)')
+plt.legend(loc='upper left')
+
+plt.show()
+
+future_Results=results.params[0]+results.params[1]*(len(x)+1)
+print(f'1日後のNASDAQ Composite Indexの予想終値は、${int(np.exp(future_Results))}です。')
+
+future_Results2=results.params[0]+results.params[1]*(len(x)+2)
+print(f'2日後のNASDAQ Composite Indexの予想終値は、${int(np.exp(future_Results2))}です。')
+
+future_Results3=results.params[0]+results.params[1]*(len(x)+3)
+print(f'3日後のNASDAQ Composite Indexの予想終値は、${int(np.exp(future_Results3))}です。')
